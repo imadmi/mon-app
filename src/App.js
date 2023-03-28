300950c4de8ca676dfd76674327cb58da18755ae
Imad Mimouni <imad.mimouni.123@gmail.com>
2023-03-28 07:54:38 +0000

    update

--- src/App.js
+++ src/App.js
@@ -3,11 +3,6 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <p>
-          
-        </p>
-      </header>
     </div>
   );
 }
