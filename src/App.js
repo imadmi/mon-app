3c2b637cd2ea5e32a448f040ef6b83960afb7a8b
Imad Mimouni <imad.mimouni.123@gmail.com>
2023-03-30 09:13:15 +0000

    update

--- src/App.js
+++ src/App.js
@@ -2,7 +2,19 @@ import './App.css';
 
 function App() {
   return (
-    <div className="App">
+    <div className="grid grid-cols-3 h-screen">
+      <div></div>
+      <div>
+        <div className="mt-20 h-5/6 bg-slate-600 flex flex-col rounded-xl p-10 text-gray-50 font-alternate" style={{ fontFamily: 'Alkatra' }}>
+          <div className="">
+            imad
+          </div>
+          <div className="">
+            lmax
+          </div>
+        </div>
+      </div>
+      <div></div>
     </div>
   );
 }
